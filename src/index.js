e01c36117da8bdb3c2bf3345c5c5cd320b570db9
Tomek <margin-zero@atcplanet.com>
2017-11-23 23:47:21 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
     }
   
+    renderSquare(i) {
+      return <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />;
+    }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext, 
+      });
+    }
+
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
@@ -87,7 +106,7 @@ class Square extends React.Component {
 
   ReactDOM.render(
     <div>
-    <Game /><ShoppingList name="Alejandro Rodrigo Municipio Hernandez" />
+    <Game /><ShoppingList name="Alejandro Rodrigo Municipio Hernandez 2" />
     </div>,
     document.getElementById('root')
   );
