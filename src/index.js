55c5b37a5259c4e79c581121860610432d83f45d
Tomek <margin-zero@atcplanet.com>
2017-11-26 21:35:23 +0100

    daily commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Board extends React.Component {
+
+    render() {
+        return (
+          <div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+              {this.renderSquare(3)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(8)}
+              {this.renderSquare(9)}
+              {this.renderSquare(10)}
+              {this.renderSquare(11)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(12)}
+              {this.renderSquare(13)}
+              {this.renderSquare(14)}
+              {this.renderSquare(15)}
+            </div>
+          </div>
+        );
+    }
+
+    renderSquare(i) {
+        return (
+            <div className="board-square">{i+1}</div>
+        )
+    }
+}
+
+
+
+class Game extends React.Component {
+
+    render() { 
+        return (
+            <Board />
+        )
+    }
+
+}
+
+// =====================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+
+// ====================================
