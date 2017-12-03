35af1b2b8928e4ae5eaca59a5a1fa041f1fefc41
Tomek <margin-zero@atcplanet.com>
2017-12-03 19:15:35 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,29 @@
-// webpack.config.js
-
 const HtmlPlugin = require('html-webpack-plugin')
-const path = require('path')
+const Path = require('path')
 
 module.exports = {
-  context: __dirname,
   // Tell webpack to start bundling our app at app/index.js
-  entry: './app',
-
+  entry: './src',
   // Output our app to the dist/ directory
   output: {
     filename: 'app.js',
     path: __dirname + '/dist'
   },
-
   // Emit source maps so we can debug our code in the browser
   devtool: 'source-map',
-
   // Tell webpack to run our source code through Babel
   module: {
-    loaders: [{
+    loaders: [
+      {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
     },
-    {
-      test: /\.css$/,
-      loader: '!style!css'
-    }]
+    { test: /\.css$/, loader: "style-loader!css-loader" }
+  ]
   },
-
-
-  resolve: {
-    extensions: [ ".webpack.js", ".web.js", ".js", ".json"]
-  },
-
   // Since Webpack only understands JavaScript, we need to
-  // add a plugin to tell it how to handle html files.   
+  // add a plugin to tell it how to handle html files.
   plugins: [
     // Configure HtmlPlugin to use our own index.html file
     // as a template.
