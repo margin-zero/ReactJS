13b2e59d3b1202c80f6eef88edbe9bce0a88f75c
Tomek <margin-zero@atcplanet.com>
2017-12-07 21:35:32 +0100

    config update

--- webpack.config/go15_dev.config.js
+++ webpack.config/go15_dev.config.js
@@ -1,28 +1,28 @@
 // Base configuration for app
 
-const dev_base = require('./_base_dev.config.js');
+const devBase = require('./_base_dev.config.js');
 var merge = require('merge'), original, cloned;
 const HtmlPlugin = require('html-webpack-plugin')
-const Path = require('path')
+const path = require('path')
 
 // entry point of the app ( module.exports.entry )
-const EntryPoint = './src___game_of_fifteen';
+const entryPoint = './src___game_of_fifteen/src';
 
 // output file configuration ( module.export.output )
-const OutputFile = {
+const outputFile = {
         filename: 'game_of_fifteen.js',
-        path: __dirname + '/../dist___game_of_fifteen' 
+        path: __dirname + '/../dist___game_of_fifteen/dev' 
     };
 
 // HtmlPlugin.template used by current app - will be added to module.exports.plugins array
-const Plugins = (
+const plugins = (
         new HtmlPlugin({
-            template: './src___game_of_fifteen/index.html'
+            template: './src___game_of_fifteen/public/index.html'
         })
     );
 
 
-module.exports = dev_base;
-module.exports.entry = EntryPoint;
-module.exports.output = OutputFile;
-module.exports.plugins.push(Plugins);
+module.exports = devBase;
+module.exports.entry = entryPoint;
+module.exports.output = outputFile;
+module.exports.plugins.push(plugins);
