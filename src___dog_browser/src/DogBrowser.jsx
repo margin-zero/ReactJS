f0db54d76470096b166eeafee9fd2141d49ce3dd
Tomek <margin-zero@atcplanet.com>
2017-12-14 23:34:30 +0100

    dog browser - daily commit

--- src___dog_browser/src/DogBrowser.jsx
+++ src___dog_browser/src/DogBrowser.jsx
@@ -1,56 +1,71 @@
 import React from 'react';
 import './index.css';
 
+import CustomSelect from './CustomSelect';
+
+
 const API = 'https://dog.ceo/api/breeds/list/all';
 
-
 export default class DogBrowser extends React.Component {
+    
+    id = 'dupa jasio ID';
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
+            allBreeds: [],
             mainBreeds: [],
-            allBreeds: []
+            selectedBreed: 'affenpinscher'
         };
     }
 
-    componentWillMount() {
-
+    componentDidMount() {
         fetch(API)
-        .then(response => response.json())
-        .then(data => this.setState({ 
-            mainBreeds: Object.keys(data.message),
-            allBreeds: Object.entries(data.message)
-         }));
-
-        //alert(this.state.breeds[0].key + " == " + this.state.breeds[0].value);
+            .then(response => response.json())
+            .then(data => this.initBreeds(data));
     }
+
+    initBreeds(data) {
+
+        var breedArray = Object.entries(data.message),
+            newAllBreeds = [],
+            newMainBreeds = [];
+
+        for (let i = 0; i < breedArray.length; i++) {
+
+            let mainBreed = breedArray[i][0];
+
+            newMainBreeds.push(mainBreed);
+
+            if (breedArray[i][1].length > 0) {
+                for (let j = 0; j < breedArray[i][1].length; j++) {
+                    newAllBreeds.push({mainBreed: mainBreed, subBreed: breedArray[i][1][j], displayBreed: breedArray[i][1][j] + ' ' + mainBreed})
+                }
+            } else {
+                newAllBreeds.push({mainBreed: mainBreed, subBreed: null, displayBreed: mainBreed})
+            }
+        }
+
+        this.setState({ 
+            allBreeds: newAllBreeds, 
+            mainBreeds: newMainBreeds,
+            selectedBreed: newMainBreeds[0]
+        });
+    }
+
 
     render() { 
 
-        var mb = "";
-
-        for (let i = 0; i < this.state.mainBreeds.length; i++) {
-
-            let ab = this.state.allBreeds;
-            
-            mb = mb + ab[i][0];
-
-            if (ab[i][1].length > 0) { 
-                mb = mb + ": " + ab[i][1] + "... <br> "
-            } else
-            { mb = mb + " (tylko main)... <br>"};
-
-
-     
-        }
-
         return (
             <div>
-                <p>This is Dog Browser application</p>
-                <p>
-                {mb}
-                </p>
+            <p>This is Dog Browser application</p>
+
+            <CustomSelect 
+                allBreeds={this.state.allBreeds}
+                mainBreeds={this.state.mainBreeds}
+                selectedBreed={this.state.selectedBreed}
+            />
+
             </div>
         )
     }
