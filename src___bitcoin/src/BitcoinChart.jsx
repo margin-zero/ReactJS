2955e8ca745fda5be2b843110929d747bc303896
Tomek <margin-zero@atcplanet.com>
2017-12-20 23:53:02 +0100

    bitcoin - daily commit

--- src___bitcoin/src/BitcoinChart.jsx
+++ src___bitcoin/src/BitcoinChart.jsx
@@ -2,14 +2,76 @@ import React from 'react';
 
 
 export default class BitcoinChart extends React.Component {
+
+    getChart() {
+        var chartObject,
+            chartCurrency = this.props.currency,
+            chartData = this.props.dataArray.map(function(x) {
+                if (x) { return x.bpi[chartCurrency].rate_float; }
+                return 0;
+            }),
+            lines = [],
+            maxValue = chartData[chartData.length-1],
+            minValue = chartData[chartData.length-1],
+            unitValue,
+            coords = {};
+
+        for (let i = 0; i < this.props.dataArray.length - 1; i ++) {
+            if (chartData !== 0) {
+                if (chartData[i] < minValue) { minValue = chartData[i] };
+                if (chartData[i] > maxValue) { maxValue = chartData[i] };
+            }
+        }
+
+        if (minValue === 0) {
+            minValue = maxValue;
+        }
+
+        if (minValue === maxValue) {
+            minValue = minValue - 100;
+            maxValue = maxValue + 100;
+        }
+
+        unitValue = (maxValue - minValue) / 201;
+
+        for (let i = 1; i < this.props.dataArray.length; i++) {
+            
+                coords.x1 = (i-1) * 20;
+                coords.x2 = i * 20;
+
+            if (chartData[i-1] > 0) {
+                coords.y1 = ((maxValue - chartData[i-1]) * unitValue).toString();
+            } else {
+                coords.y1 = 201;
+            }
+
+            if (chartData[i] > 0) {
+                coords.y2 = ((maxValue - chartData[i]) * unitValue).toString();
+            } else {
+                coords.y2 = 201;
+            }
+                lines.push(
+                    <line key={i} className="bitcoin-chart-line" {...coords} />
+                )
+            
+        }
+        
+        chartObject = <svg className="bitcoin-chart">
+                {lines}
+            </svg>;
+
+        return  chartObject;
+
+    }
+
+
     render() {
         return (
             <section>
                 <h1>Bitcoin Chart!</h1>
-                <p>chart for {this.props.currency}</p>
-            <div style={{width: '1200px', height: '200px', background: 'yellow'}}>
-                <div style={{width: '20px', height: '150px', background: 'red'}}></div>
-            </div>
+                <p>chart for {this.props.currency} :</p>
+                {this.getChart()}
+
             </section>
         )
     }
